1569452f7d6ee837d5291ca3548859b6ce55000b
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2022-07-04 17:02:27 +0530

    helmet added ㊙️

--- server.js
+++ server.js
@@ -1,13 +1,41 @@
 const path = require("path");
+const fs = require("fs");
+const https = require("https");
 const express = require("express");
+const helmet = require("helmet");
+const app = express();
+app.use(helmet());
+
 require("dotenv").config();
-
 const PORT = 3000;
-
+app.get("/secret", (req, res) => {
+  return res.send("You Personal secret number is 50");
+});
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
+app.use(helmet());
+
+https
+  .createServer(
+    {
+      key: fs.readFileSync("./key.pem"),
+      cert: fs.readFileSync("./cert.pem"),
+    },
+    app
+  )
+  .listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT} 😄 `);
+  });
+
+const get = async (url) => {
+  const response = await fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data = await response.json();
+  return data;
+};
