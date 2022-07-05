0de16dc564689ccdf7ec6fee3e14c2a7a6fdabbb
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2022-07-05 09:21:16 +0530

    google auth 2.0 added 😉

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ const https = require("https");
 const express = require("express");
 const helmet = require("helmet");
 const app = express();
-app.use(helmet());
 
+const { Strategy } = require("passport-google-oauth20");
+const passport = require("passport");
+
+app.use(helmet());
+app.use(passport.initialize());
 //! Function to check user is logged in or not
 
 const checkLoggedIn = (req, res, next) => {
@@ -16,14 +20,47 @@ const checkLoggedIn = (req, res, next) => {
   // next();
 };
 
-app.get("/auth/google", (req, res) => {});
-app.get("/auth/google/callback", (req, res) => {});
-//! Logout
-app.get("/auth/logout", (req, res) => {});
-
 require("dotenv").config();
 
 const PORT = 3000;
+
+const AUTH_OPTIONS = {
+  callbackURL: "/auth/google/callback",
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+};
+
+function verifyCallback(accessToken, refreshToken, profile, done) {
+  console.log("Profile 🧑‍🦯", profile);
+  done(null, profile);
+}
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  }),
+  (req, res) => {
+    console.log("Redirecting to Google");
+  }
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/",
+    successRedirect: "/",
+    session: false,
+  }),
+  (req, res) => {
+    console.log("Redirecting to Google");
+  }
+);
+//! Logout
+app.get("/auth/logout", (req, res) => {});
+
 app.get("/secret", checkLoggedIn, (req, res) => {
   return res.send("You Personal secret number is 50");
 });
@@ -43,7 +80,5 @@ https
     app
   )
   .listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT} 😄 `);
+    console.log(`Server is listening on port ${PORT} 🚀 `);
   });
-
-console.log(process.env.GOOGLE_CLIENT_ID);
